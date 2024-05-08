3a7d3759aeda4bd8e864c7beccc7e7c486aa4279
Ayo <Ayo.meshe@gmail.com>
2024-05-08 13:40:01 +0100

    project initialisation

---
+++ src/middleware.ts
@@ -0,0 +1,12 @@
+import { NextRequest } from "next/server";
+
+export function middleware(req: NextRequest) {
+  if (req.nextUrl.pathname) {
+    console.log("TRACK");
+  }
+}
+
+// See "Matching Paths" below to learn more
+export const matcher = {
+  matcher: ["/"],
+};
