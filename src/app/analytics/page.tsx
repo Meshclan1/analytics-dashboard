c141da2b483e69ee02e90f29a24f6c027bc2563a
Ayo <Ayo.meshe@gmail.com>
2024-05-09 12:36:09 +0100

    setting up calculation formula

--- src/app/analytics/page.tsx
+++ src/app/analytics/page.tsx
@@ -3,12 +3,27 @@ import { analytics } from "@/utils/analytics";
 import AnalyticsDashboard from "../components/AnalyticsDashboard";
 
 const Page = async () => {
-  const pageview = await analytics.retrieveDays("pageview", 2);
+  // all caps indicates that this is a fixed value
+  const TRACKING_DAYS = 7;
+
+  const pageviews = await analytics.retrieveDays("pageview", TRACKING_DAYS);
+
+  // we go into each day, then go into each event
+  const totalPageViews = pageviews.reduce((acc, curr) => {
+    return (
+      acc +
+      curr.events.reduce((acc, curr) => {
+        return acc + Object.values(curr)[0]!;
+      }, 0)
+    );
+  }, 0);
+
+  const avgVisitorsPerDay = (totalPageViews / TRACKING_DAYS).toFixed(1);
 
   return (
     <div className="min-h-screen w-full py-12 flex justify-center items-center">
       <div className="relative w-full max-w-6xl mx-auto text-white">
-        <AnalyticsDashboard />
+        <AnalyticsDashboard avgVisitorsPerDay={avgVisitorsPerDay} />
       </div>
     </div>
   );
