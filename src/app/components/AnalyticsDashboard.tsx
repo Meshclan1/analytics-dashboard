84767bf4954ee3ad0c175b340e21af0191cacc50
Ayo <Ayo.meshe@gmail.com>
2024-05-09 12:17:43 +0100

    install and setup tremor npm

---
+++ src/app/components/AnalyticsDashboard.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+type Props = {};
+
+const AnalyticsDashboard = (props: Props) => {
+  return (
+    <div className="flex flex-col gap-6">
+      <div className="grid w-full mx-auto grid-cols-1 sm:grid-cols-2 gap-6"></div>
+    </div>
+  );
+};
+
+export default AnalyticsDashboard;
