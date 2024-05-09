7fefaff5d4117817da2f578969ca4c753d8305d9
Ayo <Ayo.meshe@gmail.com>
2024-05-09 12:42:18 +0100

    update analytics dashboard

--- src/app/components/AnalyticsDashboard.tsx
+++ src/app/components/AnalyticsDashboard.tsx
@@ -5,9 +5,13 @@ import React from "react";
 
 type AnalyticsDashboardProps = {
   avgVisitorsPerDay: string;
+  visitorAmountToday: number;
 };
 
-const AnalyticsDashboard = ({ avgVisitorsPerDay }: AnalyticsDashboardProps) => {
+const AnalyticsDashboard = ({
+  avgVisitorsPerDay,
+  visitorAmountToday,
+}: AnalyticsDashboardProps) => {
   return (
     <div className="flex flex-col gap-6">
       <div className="grid w-full mx-auto grid-cols-1 sm:grid-cols-2 gap-6">
@@ -17,6 +21,14 @@ const AnalyticsDashboard = ({ avgVisitorsPerDay }: AnalyticsDashboardProps) => {
           </p>
           <p className="text-3xl text-dark-tremor-content-strong font-semibold">
             {avgVisitorsPerDay}
+          </p>
+        </Card>
+        <Card className="w-full mx-auto max-w-xs">
+          <p className="text-tremor-default text-dark-tremor-content">
+            Visitors Today
+          </p>
+          <p className="text-3xl text-dark-tremor-content-strong font-semibold">
+            {visitorAmountToday}
           </p>
         </Card>
       </div>
